72aa1c00b66a0b252d076a2bcf774cc9be419b81
PRIYA KUMARI <priyakumarirai.012345@gmail.com>
2025-02-19 14:49:38 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,5 +1,5 @@
 const server = process.env.NODE_ENV === "production" 
-    ? "https://zoomclonebackend-wz6f.onrender.com" 
+    ? "https://zoom-clonebackend-lzr8.onrender.com" 
     : "http://localhost:8000";
 
 export default server;
